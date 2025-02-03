77952e92851322218df82c5d870b83fdfcd8a24f
Gor <arakelyangor10@gmail.com>
2025-02-04 00:23:33 +0400

    Implement service sync initialization interface

--- scriptable/service/service.py
+++ scriptable/service/service.py
@@ -4,8 +4,7 @@ from .base import ServiceMeta
 from .handlers.composer.composer_async import ServiceComposer as ServiceAsyncComposer
 from .handlers.composer.composer_sync import ServiceComposer as ServiceSyncComposer
 from .handlers.initializer.initializer_async import ServiceInitializer as ServiceAsyncInitializer
-
-# from .handlers.initializer.initializer_sync import ServiceInitializer as ServiceSyncInitializer
+from .handlers.initializer.initializer_sync import ServiceInitializer as ServiceSyncInitializer
 
 
 class AsyncService(
@@ -18,6 +17,7 @@ class AsyncService(
 
 class SyncService(
     ServiceSyncComposer,
+    ServiceSyncInitializer,
     metaclass=ServiceMeta,
 ):
     pass
