0ebe3d6108b276e5f3cba625497d9479d2635aa7
Gor <arakelyangor10@gmail.com>
2025-10-06 17:15:14 +0400

    chore: Merge root subpackages into one src dir (src/redwood)

--- src/loomistd/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Loomistd: Loomi Standard Library
-This module provides a collection of standard functions and classes
-for the Loomi project. It includes utilities for state management, operation
-execution, and other common tasks.
-"""
